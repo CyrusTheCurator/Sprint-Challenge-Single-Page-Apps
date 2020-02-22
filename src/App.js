723b45461da8a41012c2c471ffa0beaa0c013957
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-02-21 18:18:48 -0800

    setting up initial formats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
 import Header from "./components/Header.js";
-
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route
+        path="/"
+        render={props => {
+          return <SearchForm {...props} />;
+        }}
+      />
+      <Route
+        exact
+        path="/"
+        render={props => {
+          return <WelcomePage {...props} />;
+        }}
+      />
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
- 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+    console.log(searchTerm);
+  };
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <label htmlFor="name">Search:</label>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </form>
     </section>
   );
 }
